4d0af5732222b7fbcb5c5c1572e7f208f6e13fc2
ajeong4032 <dkwjd2351@naver.com>
2025-02-19 14:18:08 +0900

    script 요소

---
+++ src/html/js/23-script.js
@@ -0,0 +1,15 @@
+document.querySelector('.flip-button').addEventListener('click', () => {
+  document.querySelector('.card').classList.toggle('is-flipped');
+});
+// <script type="module">
+//document.addEventListener('DOMContentLoaded', () => {
+// const flipButton = document.querySelector('.flip-button');
+//  const card = document.querySelector('.card');
+
+// const flipCard = () => {
+//    card.classList.toggle('is-flipped');
+//  };
+
+// flipButton.addEventListener('click', flipCard);
+//});
+//</script>
